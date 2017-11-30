d9cdf7805548323d43be4d17ec43717c77309427
stephdet <sdethy@gmail.com>
2017-11-30 09:29:50 +0100

    add

---
+++ exercices.js
@@ -0,0 +1,14 @@
+var animal = function(nom){
+this.nom=nom;
+this.pattes=4;
+this.cri="ahhhhhh";
+return "le cri"+this.cri;
+console.log(+crabe.nom+);
+
+}
+
+// var biche = new animal("labelle");
+//
+// console.log(biche.pattes);
+var biche =new animal("croquette");
+var crabe=new animal("homard");
